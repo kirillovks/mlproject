09b4ba79446eadb07c65dddba7e92967935e5a7a
kirillovks <kir.kirillo2002@yandex.ru>
2023-09-25 13:42:45 +0700

    added instructions to git and cmd

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello, it's test")
+# some git instructions:
+#   git clone ...
+#   git status
+#   git add ...
+#   git commit -m "text of the commit"
+#   git push
+#   git pull
+# cmd commands:
+#   cd Documents - change directory
+#   cd.. - return to previous directory
+#   dir - check what's inside
+
+print("everything is working")
