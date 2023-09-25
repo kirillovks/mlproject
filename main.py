294ae359ae01550d79706fdb4d0ce9f28ca1a0b9
kirillovks <kir.kirillo2002@yandex.ru>
2023-09-25 13:59:19 +0700

    some new info about git

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # some git instructions:
+#   git init
 #   git clone ...
 #   git status
 #   git add ...
 #   git commit -m "text of the commit"
 #   git push
 #   git pull
+#   add file .gitignore to ur repository to ignore committing files that described in .gitignore
 # cmd commands:
 #   cd Documents - change directory
 #   cd.. - return to previous directory
